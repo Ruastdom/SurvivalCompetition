503c9cffbd511dcae46ef986be682e888fd5ca66
MATRIX-feather <midnightcarnival@outlook.com>
2022-08-09 16:00:55 +0800

    fix: ...
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- src/main/java/xiamomc/survivalcompetition/SurvivalCompetition.java
+++ src/main/java/xiamomc/survivalcompetition/SurvivalCompetition.java
@@ -69,6 +69,7 @@ public final class SurvivalCompetition extends JavaPlugin {
     private void tick()
     {
         runnables.forEach(Runnable::run);
+        runnables.clear();
     }
 
     private final List<Runnable> runnables = new ArrayList<Runnable>();
